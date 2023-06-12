073204619b388b539f4c7abdea380237e6d6f109
Tobias Hoefken <Tobizzzzle@gmail.com>
2023-06-12 14:18:33 +0200

    [TASK] findNextDeparturesByTime returns departures within next hour

--- public/departureController.php
+++ public/departureController.php
@@ -4,13 +4,16 @@ $username = 'root';
 $password = 'root';
 
 $conn = new PDO($dsn, $username, $password);
+
 class departureController
 {
-    function findNextDeparturesByTime($time)
+    public function findNextDeparturesByTime($time)
     {
         global $conn;
-        $stmt = $conn->prepare('SELECT * FROM departure WHERE time>:time');
+        $timePlusOne = date(' H:i:s', strtotime('now +1 hour'));
+        $stmt = $conn->prepare('SELECT * FROM departure WHERE time>:time AND time<:timePlusOne');
         $stmt->bindParam(':time', $time);
+        $stmt->bindParam(':timePlusOne', $timePlusOne);
         $stmt->execute();
 
         return $stmt->fetchAll();
