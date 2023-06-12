cc45659634012abf50418802a72f2f3314cfced3
Tobias Hoefken <Tobizzzzle@gmail.com>
2023-06-12 12:12:46 +0200

    [TASK] implement departureController.php [TASK] removed departure.php

--- public/departureController.php
+++ public/departureController.php
@@ -1,1 +1,18 @@
 <?php
+$dsn = 'mysql:dbname=db;host=ddev-departures-db:3306';
+$username = 'root';
+$password = 'root';
+
+$conn = new PDO($dsn, $username, $password);
+class departureController
+{
+    function findNextDepartureByTime( $time)
+    {
+        global $conn;
+        $stmt = $conn->prepare('SELECT * FROM departure WHERE time=:time');
+        $stmt->bindParam(':time', $time);
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+    }
+}
